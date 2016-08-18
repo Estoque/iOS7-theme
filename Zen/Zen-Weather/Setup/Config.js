17c6e1f2cb8b1b707456c23a7d2883ae893ac594
Estoque <car6754@gmail.com>
2016-08-18 18:14:33 +0900

    Add files via upload

---
+++ Zen/Zen-Weather/Setup/Config.js
@@ -0,0 +1,8 @@
+// Configuration here ! //
+// By Dacal for Subywrex
+// Adaptation for Native Weather
+var ampm = false; // Set to true for 12h format
+var updateInterval = 15; // in minutes
+var lang = "fr"; // (fr) for french, (de) for german, (sp) for spanish, (it) for italian, (en) or anything else for english,(am) for us
+var SecDisplay = false; // true to display seconds - battery eater !
+var AnimatedWeather = false; // false for static weather images only.
